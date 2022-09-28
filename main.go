f54bc079d21acb50903e74e80ef005f71ee75b2d
Akashkumar-Jeyaramans <jeyakashkumar@gmail.com>
2022-09-28 20:20:44 +0530

    working fine

--- main.go
+++ main.go
@@ -2,20 +2,32 @@ package main
 
 import (
 	"context"
-	gen "grpc-gateway-demo/gen/go/hello"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
 
+	gen "github.com/Akashkumar-Jeyaramans/grpcGateway/v1/commands"
+
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
-	"google.golang.org/grpc"
 )
+
+type snmpgrpcServer struct {
+	gen.UnimplementedGreeterServer
+}
+
+func (s *snmpgrpcServer) SayHello(ctx context.Context, req *gen.HelloRequest) (*gen.HelloReply, error) {
+	return &gen.HelloReply{
+		Message: fmt.Sprintf("hello %s ", req.Name),
+	}, nil
+}
 
 func main() {
 	// creating mux for gRPC gateway. This will multiplex or route request different gRPC service
 	mux := runtime.NewServeMux()
 	// setting up a dail up for gRPC service by specifying endpoint/target url
-	err := gen.RegisterGreeterHandlerFromEndpoint(context.Background(), mux, "localhost:8080", []grpc.DialOption{grpc.WithInsecure()})
+	//err := gen.RegisterGreeterHandlerFromEndpoint(context.Background(), mux, "localhost:50051", []grpc.DialOption{grpc.WithInsecure()})
+	err := gen.RegisterGreeterHandlerServer(context.Background(), mux, &snmpgrpcServer{})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,7 +36,7 @@ func main() {
 		Handler: mux,
 	}
 	// creating a listener for server
-	l, err := net.Listen("tcp", ":8081")
+	l, err := net.Listen("tcp", ":8083")
 	if err != nil {
 		log.Fatal(err)
 	}
